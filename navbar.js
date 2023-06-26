da75a8e3965cdfbce73d9d29126a37753d52055c
Vitaly <vitaly.varela@hotmal.com>
2023-06-26 16:41:17 -0600

    falta de ortografia

--- navbar.js
+++ navbar.js
@@ -1,7 +1,5 @@
 const menu = document.getElementById('open-menu');
 
-
-
 menu.addEventListener('click', () => {
-    document.getElementById('header-toogle').classList.toggle('active');
+    document.getElementById('header-toggle').classList.toggle('active');
 })
