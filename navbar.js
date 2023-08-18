5b9ba50f75da6a0efd131907e1a5d361d7a0ca73
Vitaly <vitaly.varela@hotmail.com>
2023-08-17 18:15:16 -0600

    add buttom leer mas a cads

--- navbar.js
+++ navbar.js
@@ -1,5 +1,31 @@
 const menu = document.getElementById('open-menu');
 
+const inputCard = document.getElementById('input-card');
+const inputCardConsejo = document.getElementById('input-card-consejo');
+const inputCardServices = document.getElementById('input-card-services');
+
+// const spanCardInput = document.getElementById('span-card-input');
+
 menu.addEventListener('click', () => {
     document.getElementById('header-toggle').classList.toggle('active');
+})
+
+
+// INPUTS-CARDS**********
+
+
+inputCardServices.addEventListener('click', () => {
+    document.getElementById('span-card-input-services').classList.toggle('span-hidden')
+    document.getElementById('list-services').classList.toggle('input-open-card');
+})
+
+inputCard.addEventListener('click', () => {
+    document.getElementById('span-card-input-nosotros').classList.toggle('span-hidden')
+    document.getElementById('confia-en-nosotros').classList.toggle('input-open-card');
+})
+
+inputCardConsejo.addEventListener('click', () => {
+    document.getElementById('span-card-input-consejo').classList.toggle('span-hidden')
+    document.getElementById('un-consejo').classList.toggle('input-open-card');
+
 })
