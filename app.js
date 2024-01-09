5689f4271b4081ec889ec6b8b48631af0749a9c3
Vitaly <vitaly.varela@hotmail.com>
2024-01-09 17:26:13 -0600

    number and scrollbar update

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
 const menu = document.getElementById('open-menu');
-
 const inputCard = document.getElementById('input-card');
 const inputCardConsejo = document.getElementById('input-card-consejo');
 const inputCardServices = document.getElementById('input-card-services');
 let more = document.querySelectorAll('.more')
 
 
-for (let i = 0; i <more.length; i++) {
-    more[i].addEventListener('click', function(){
-        more[i].classList.toggle('more-bg')
-        more[i].classList.toggle('active_card')
-    })
-}
-
 // BOTON MENU
 menu.addEventListener('click', () => {
     document.getElementById('header-toggle').classList.toggle('active');
 })
 
-
-// INPUTS-CARDS**********
-
-
+// INPUTS-CARDS ABRIR Y CERAR / TEXTO NO HIDDEN**********
 inputCardServices.addEventListener('click', () => {
     document.getElementById('span-card-input-services').classList.toggle('span-hidden')
     document.getElementById('list-services').classList.toggle('input-open-card');
@@ -35,8 +24,13 @@ inputCard.addEventListener('click', () => {
 inputCardConsejo.addEventListener('click', () => {
     document.getElementById('span-card-input-consejo').classList.toggle('span-hidden')
     document.getElementById('un-consejo').classList.toggle('input-open-card');
-
-
 })
 
+// BOTOM CAMBIO DE COLOR / CAMBIAR TEXTO ********
+for (let i = 0; i < more.length; i++) {
+    more[i].addEventListener('click', function(){
+        more[i].classList.toggle('more-bg')
+        more[i].classList.toggle('active_card')
+    })
+}
 
