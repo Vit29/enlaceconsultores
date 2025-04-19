69757a214275445e2968eaee8a40425bebd7713b
Vitaly <vitaly.varela@hotmail.com>
2025-04-18 19:06:18 -0600

    scroll animation

--- app.js
+++ app.js
@@ -1,36 +1,59 @@
-const menu = document.getElementById('open-menu');
-const inputCard = document.getElementById('input-card');
-const inputCardConsejo = document.getElementById('input-card-consejo');
-const inputCardServices = document.getElementById('input-card-services');
-let more = document.querySelectorAll('.more')
+const menu = document.getElementById("open-menu");
+const inputCard = document.getElementById("input-card");
+const inputCardConsejo = document.getElementById("input-card-consejo");
+const inputCardServices = document.getElementById("input-card-services");
+const sectionNosotros = document.getElementById("quienes-somos");
+const sectionNustrosServicios = document.getElementById("nuestros-servicios");
+const sectionVideoInfo = document.getElementById("video-info");
+const sectionContact = document.getElementById("contac");
 
+let more = document.querySelectorAll(".more");
 
 // BOTON MENU
-menu.addEventListener('click', () => {
-    document.getElementById('header-toggle').classList.toggle('active');
-})
+menu.addEventListener("click", () => {
+  document.getElementById("header-toggle").classList.toggle("active");
+});
 
 // INPUTS-CARDS ABRIR Y CERAR / TEXTO NO HIDDEN**********
-inputCardServices.addEventListener('click', () => {
-    document.getElementById('span-card-input-services').classList.toggle('span-hidden')
-    document.getElementById('list-services').classList.toggle('input-open-card');
-})
+inputCardServices.addEventListener("click", () => {
+  document
+    .getElementById("span-card-input-services")
+    .classList.toggle("span-hidden");
+  document.getElementById("list-services").classList.toggle("input-open-card");
+});
 
-inputCard.addEventListener('click', () => {
-    document.getElementById('span-card-input-nosotros').classList.toggle('span-hidden')
-    document.getElementById('confia-en-nosotros').classList.toggle('input-open-card');
-})
+inputCard.addEventListener("click", () => {
+  document
+    .getElementById("span-card-input-nosotros")
+    .classList.toggle("span-hidden");
+  document
+    .getElementById("confia-en-nosotros")
+    .classList.toggle("input-open-card");
+});
 
-inputCardConsejo.addEventListener('click', () => {
-    document.getElementById('span-card-input-consejo').classList.toggle('span-hidden')
-    document.getElementById('un-consejo').classList.toggle('input-open-card');
-})
+inputCardConsejo.addEventListener("click", () => {
+  document
+    .getElementById("span-card-input-consejo")
+    .classList.toggle("span-hidden");
+  document.getElementById("un-consejo").classList.toggle("input-open-card");
+});
 
 // BOTOM CAMBIO DE COLOR / CAMBIAR TEXTO ********
 for (let i = 0; i < more.length; i++) {
-    more[i].addEventListener('click', function(){
-        more[i].classList.toggle('more-bg')
-        more[i].classList.toggle('active_card')
-    })
+  more[i].addEventListener("click", function () {
+    more[i].classList.toggle("more-bg");
+    more[i].classList.toggle("active_card");
+  });
 }
 
+const sizeHeight = window.innerHeight;
+const sizeWidth = window.innerWidth;
+
+window.addEventListener("scroll", () => {
+  const section = Math.round(scrollY / sizeHeight);
+
+  if (section == 1) sectionNosotros.classList.add("show");
+  if (section == 2) sectionNustrosServicios.classList.add("show");
+  if (section == 3) sectionVideoInfo.classList.add("show");
+  if (section == 4) sectionContact.classList.add("show");
+});
